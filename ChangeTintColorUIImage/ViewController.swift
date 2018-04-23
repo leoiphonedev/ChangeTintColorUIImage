a783ae7b010182f50bdaef47c775c383c1ea67be
leoiphonedev <aggarwalaman1985@gmail.com>
2018-04-24 00:11:06 +0530

    Pushing code to GitHub

--- ChangeTintColorUIImage/ViewController.swift
+++ ChangeTintColorUIImage/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgvBuilding: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.perform(#selector(changeImageColorToYellow), with: nil, afterDelay: 2.5)
+    }
+    
+    @objc func changeImageColorToYellow (){
+     let templateImage = imgvBuilding.image?.withRenderingMode(.alwaysTemplate)
+       imgvBuilding.image = templateImage
+        imgvBuilding.tintColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
